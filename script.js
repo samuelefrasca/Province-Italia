3d763e063e2010a925101b29f4d0f69df0432862
Samuele Frasca <samufra06@gmail.com>
2026-04-18 18:45:39 +0200

    Add population order and icon to the site

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const provinceItalia = {
     "Valle d'Aosta": [
-        { nome: "Aosta", sigla: "AO", abitanti: 122554, metropolitana: false }
+        { nome: "Aosta", sigla: "AO", abitanti: 122554, metropolitana: false, capoluogo: true }
     ],
     "Piemonte": [
-        { nome: "Torino", sigla: "TO", abitanti: 2204779, metropolitana: true },
         { nome: "Alessandria", sigla: "AL", abitanti: 408063, metropolitana: false },
         { nome: "Asti", sigla: "AT", abitanti: 207059, metropolitana: false },
         { nome: "Biella", sigla: "BI", abitanti: 168223, metropolitana: false },
         { nome: "Cuneo", sigla: "CN", abitanti: 582109, metropolitana: false },
         { nome: "Novara", sigla: "NO", abitanti: 365930, metropolitana: false },
+        { nome: "Torino", sigla: "TO", abitanti: 2204779, metropolitana: true, capoluogo: true },
         { nome: "Verbano-Cusio-Ossola", sigla: "VB", abitanti: 152677, metropolitana: false },
         { nome: "Vercelli", sigla: "VC", abitanti: 166166, metropolitana: false }
     ],
     "Liguria": [
-        { nome: "Genova", sigla: "GE", abitanti: 820691, metropolitana: true },
+        { nome: "Genova", sigla: "GE", abitanti: 820691, metropolitana: true, capoluogo: true },
         { nome: "Imperia", sigla: "IM", abitanti: 209848, metropolitana: false },
         { nome: "La Spezia", sigla: "SP", abitanti: 214539, metropolitana: false },
         { nome: "Savona", sigla: "SV", abitanti: 266910, metropolitana: false }
     ],
     "Lombardia": [
-        { nome: "Milano", sigla: "MI", abitanti: 3253111, metropolitana: true },
         { nome: "Bergamo", sigla: "BG", abitanti: 1119233, metropolitana: false },
         { nome: "Brescia", sigla: "BS", abitanti: 1271759, metropolitana: false },
         { nome: "Como", sigla: "CO", abitanti: 598562, metropolitana: false },
         { nome: "Cremona", sigla: "CR", abitanti: 355134, metropolitana: false },
         { nome: "Lecco", sigla: "LC", abitanti: 334211, metropolitana: false },
         { nome: "Lodi", sigla: "LO", abitanti: 231481, metropolitana: false },
+        { nome: "Milano", sigla: "MI", abitanti: 3253111, metropolitana: true, capoluogo: true },
         { nome: "Mantova", sigla: "MN", abitanti: 408715, metropolitana: false },
         { nome: "Monza e Brianza", sigla: "MB", abitanti: 883396, metropolitana: false },
         { nome: "Pavia", sigla: "PV", abitanti: 546023, metropolitana: false },
@@ -34,25 +34,25 @@ const provinceItalia = {
     ],
     "Trentino-Alto Adige": [
         { nome: "Bolzano", sigla: "BZ", abitanti: 542134, metropolitana: false },
-        { nome: "Trento", sigla: "TN", abitanti: 548684, metropolitana: false }
+        { nome: "Trento", sigla: "TN", abitanti: 548684, metropolitana: false, capoluogo: true }
     ],
     "Veneto": [
-        { nome: "Venezia", sigla: "VE", abitanti: 834077, metropolitana: true },
         { nome: "Belluno", sigla: "BL", abitanti: 197265, metropolitana: false },
         { nome: "Padova", sigla: "PD", abitanti: 934540, metropolitana: false },
         { nome: "Rovigo", sigla: "RO", abitanti: 227183, metropolitana: false },
         { nome: "Treviso", sigla: "TV", abitanti: 878341, metropolitana: false },
+        { nome: "Venezia", sigla: "VE", abitanti: 834077, metropolitana: true, capoluogo: true },
         { nome: "Verona", sigla: "VR", abitanti: 930842, metropolitana: false },
         { nome: "Vicenza", sigla: "VI", abitanti: 855212, metropolitana: false }
     ],
     "Friuli Venezia Giulia": [
-        { nome: "Trieste", sigla: "TS", abitanti: 227840, metropolitana: false },
         { nome: "Gorizia", sigla: "GO", abitanti: 138717, metropolitana: false },
         { nome: "Pordenone", sigla: "PN", abitanti: 311114, metropolitana: false },
+        { nome: "Trieste", sigla: "TS", abitanti: 227840, metropolitana: false, capoluogo: true },
         { nome: "Udine", sigla: "UD", abitanti: 515825, metropolitana: false }
     ],
     "Emilia-Romagna": [
-        { nome: "Bologna", sigla: "BO", abitanti: 1024290, metropolitana: true },
+        { nome: "Bologna", sigla: "BO", abitanti: 1024290, metropolitana: true, capoluogo: true },
         { nome: "Ferrara", sigla: "FE", abitanti: 340482, metropolitana: false },
         { nome: "Forlì-Cesena", sigla: "FC", abitanti: 394337, metropolitana: false },
         { nome: "Modena", sigla: "MO", abitanti: 711502, metropolitana: false },
@@ -63,8 +63,8 @@ const provinceItalia = {
         { nome: "Rimini", sigla: "RN", abitanti: 341244, metropolitana: false }
     ],
     "Toscana": [
-        { nome: "Firenze", sigla: "FI", abitanti: 988494, metropolitana: true },
         { nome: "Arezzo", sigla: "AR", abitanti: 333269, metropolitana: false },
+        { nome: "Firenze", sigla: "FI", abitanti: 988494, metropolitana: true, capoluogo: true },
         { nome: "Grosseto", sigla: "GR", abitanti: 214863, metropolitana: false },
         { nome: "Livorno", sigla: "LI", abitanti: 324893, metropolitana: false },
         { nome: "Lucca", sigla: "LU", abitanti: 379957, metropolitana: false },
@@ -75,42 +75,42 @@ const provinceItalia = {
         { nome: "Siena", sigla: "SI", abitanti: 259502, metropolitana: false }
     ],
     "Umbria": [
-        { nome: "Perugia", sigla: "PG", abitanti: 636147, metropolitana: false },
+        { nome: "Perugia", sigla: "PG", abitanti: 636147, metropolitana: false, capoluogo: true },
         { nome: "Terni", sigla: "TR", abitanti: 214734, metropolitana: false }
     ],
     "Marche": [
-        { nome: "Ancona", sigla: "AN", abitanti: 461685, metropolitana: false },
+        { nome: "Ancona", sigla: "AN", abitanti: 461685, metropolitana: false, capoluogo: true },
         { nome: "Ascoli Piceno", sigla: "AP", abitanti: 200202, metropolitana: false },
         { nome: "Fermo", sigla: "FM", abitanti: 166827, metropolitana: false },
         { nome: "Macerata", sigla: "MC", abitanti: 301668, metropolitana: false },
         { nome: "Pesaro e Urbino", sigla: "PU", abitanti: 349578, metropolitana: false }
     ],
     "Lazio": [
-        { nome: "Roma", sigla: "RM", abitanti: 4227295, metropolitana: true },
         { nome: "Frosinone", sigla: "FR", abitanti: 460598, metropolitana: false },
         { nome: "Latina", sigla: "LT", abitanti: 567222, metropolitana: false },
         { nome: "Rieti", sigla: "RI", abitanti: 149432, metropolitana: false },
+        { nome: "Roma", sigla: "RM", abitanti: 4227295, metropolitana: true, capoluogo: true },
         { nome: "Viterbo", sigla: "VT", abitanti: 307849, metropolitana: false }
     ],
     "Abruzzo": [
-        { nome: "L'Aquila", sigla: "AQ", abitanti: 286808, metropolitana: false },
         { nome: "Chieti", sigla: "CH", abitanti: 369311, metropolitana: false },
+        { nome: "L'Aquila", sigla: "AQ", abitanti: 286808, metropolitana: false, capoluogo: true },
         { nome: "Pescara", sigla: "PE", abitanti: 311668, metropolitana: false },
         { nome: "Teramo", sigla: "TE", abitanti: 299919, metropolitana: false }
     ],
     "Molise": [
-        { nome: "Campobasso", sigla: "CB", abitanti: 207948, metropolitana: false },
+        { nome: "Campobasso", sigla: "CB", abitanti: 207948, metropolitana: false, capoluogo: true },
         { nome: "Isernia", sigla: "IS", abitanti: 78282, metropolitana: false }
     ],
     "Campania": [
         { nome: "Avellino", sigla: "AV", abitanti: 393292, metropolitana: false },
         { nome: "Benevento", sigla: "BN", abitanti: 258105, metropolitana: false },
         { nome: "Caserta", sigla: "CE", abitanti: 909469, metropolitana: false },
-        { nome: "Napoli", sigla: "NA", abitanti: 2955709, metropolitana: true },
+        { nome: "Napoli", sigla: "NA", abitanti: 2955709, metropolitana: true, capoluogo: true },
         { nome: "Salerno", sigla: "SA", abitanti: 1053616, metropolitana: false }
     ],
     "Puglia": [
-        { nome: "Bari", sigla: "BA", abitanti: 1218345, metropolitana: true },
+        { nome: "Bari", sigla: "BA", abitanti: 1218345, metropolitana: true, capoluogo: true },
         { nome: "Barletta-Andria-Trani", sigla: "BT", abitanti: 376022, metropolitana: false },
         { nome: "Brindisi", sigla: "BR", abitanti: 373790, metropolitana: false },
         { nome: "Foggia", sigla: "FG", abitanti: 588112, metropolitana: false },
@@ -118,29 +118,29 @@ const provinceItalia = {
         { nome: "Taranto", sigla: "TA", abitanti: 547928, metropolitana: false }
     ],
     "Basilicata": [
-        { nome: "Potenza", sigla: "PZ", abitanti: 337858, metropolitana: false },
-        { nome: "Matera", sigla: "MT", abitanti: 187899, metropolitana: false }
+        { nome: "Matera", sigla: "MT", abitanti: 187899, metropolitana: false },
+        { nome: "Potenza", sigla: "PZ", abitanti: 337858, metropolitana: false, capoluogo: true }
     ],
     "Calabria": [
-        { nome: "Catanzaro", sigla: "CZ", abitanti: 338285, metropolitana: false },
+        { nome: "Catanzaro", sigla: "CZ", abitanti: 338285, metropolitana: false, capoluogo: true },
         { nome: "Cosenza", sigla: "CS", abitanti: 667643, metropolitana: false },
         { nome: "Crotone", sigla: "KR", abitanti: 161820, metropolitana: false },
         { nome: "Reggio Calabria", sigla: "RC", abitanti: 510818, metropolitana: true },
         { nome: "Vibo Valentia", sigla: "VV", abitanti: 150041, metropolitana: false }
     ],
     "Sicilia": [
-        { nome: "Palermo", sigla: "PA", abitanti: 1195772, metropolitana: true },
         { nome: "Agrigento", sigla: "AG", abitanti: 407250, metropolitana: false },
         { nome: "Caltanissetta", sigla: "CL", abitanti: 243714, metropolitana: false },
         { nome: "Catania", sigla: "CT", abitanti: 1067993, metropolitana: true },
         { nome: "Enna", sigla: "EN", abitanti: 151621, metropolitana: false },
         { nome: "Messina", sigla: "ME", abitanti: 594412, metropolitana: true },
+        { nome: "Palermo", sigla: "PA", abitanti: 1195772, metropolitana: true, capoluogo: true },
         { nome: "Ragusa", sigla: "RG", abitanti: 323144, metropolitana: false },
         { nome: "Siracusa", sigla: "SR", abitanti: 382529, metropolitana: false },
         { nome: "Trapani", sigla: "TP", abitanti: 410713, metropolitana: false }
     ],
     "Sardegna": [
-        { nome: "Cagliari", sigla: "CA", abitanti: 542470, metropolitana: true },
+        { nome: "Cagliari", sigla: "CA", abitanti: 542470, metropolitana: true, capoluogo: true },
         { nome: "Gallura Nord-Est Sardegna", sigla: "OT", abitanti: 158916, metropolitana: false },
         { nome: "Medio Campidano", sigla: "VS", abitanti: 90694, metropolitana: false },
         { nome: "Nuoro", sigla: "NU", abitanti: 194616, metropolitana: false },
@@ -155,40 +155,94 @@ const regioni = document.querySelectorAll('path');
 const titoloRegione = document.getElementById('titolo-regione');
 const elencoProvince = document.getElementById('elenco-province');
 
+function scritturaProvince(nomeRegione, provinceTrovate) {
+    elencoProvince.innerHTML = ''; // Svuota la lista vecchia
+    if (elencoProvince.innerHTML == '') {
+        elencoProvince.style.border = 'none'
+    }
+    else {
+        elencoProvince.style.border = "1px solid black"
+    }
+
+    if (provinceTrovate) {
+        // Se trova i dati, cambia il titolo
+        titoloRegione.textContent = "Province della regione " + nomeRegione + ":";
+
+        // Crea i punti elenco per ogni provincia
+        let table = elencoProvince.innerHTML
+        table = `<tr><th class="el pointer" onclick="ordinaPerNome(flagAlfabetico)">Nome</th><th class="el">Sigla</th><th class="el pointer" onclick="ordinaPerAbitanti(flagAbitanti)">Popolazione</th></tr>`
+        for (let provincia of provinceTrovate) {
+            if (provincia.capoluogo) {
+                table += `<tr><td class="el nome"><strong>${provincia.nome}</strong></td><td class="el sigla"><strong>${provincia.sigla}</strong></td><td class="el abitanti"><strong>${provincia.abitanti.toLocaleString('it-IT')}</strong></td></tr>`;
+            }
+            else {
+                table += `<tr><td class="el nome">${provincia.nome}</td><td class="el sigla">${provincia.sigla}</td><td class="el abitanti">${provincia.abitanti.toLocaleString('it-IT')}</td></tr>`;
+            }
+        }
+        elencoProvince.innerHTML = table
+
+    } else {
+        // Se clicchi una regione non ancora presente nel database
+        titoloRegione.textContent = nomeRegione + " (Dati da inserire)";
+    }
+}
+
+let regioneCorrente = "";
+let provinceCorrenti = [];
+let flagAbitanti = false;
+let flagAlfabetico = true;
+
 regioni.forEach(regione => {
     regione.addEventListener('click', () => {
 
         const nomeRegione = regione.getAttribute('name'); // Legge il nome dall'SVG
+        regioneCorrente = nomeRegione;
         const provinceTrovate = provinceItalia[nomeRegione]; // Cerca il nome nel Database
-
-        elencoProvince.innerHTML = ''; // Svuota la lista vecchia
-        if (elencoProvince.innerHTML == '') {
-            elencoProvince.style.border = 'none'
-        }
-        else {
-            elencoProvince.style.border = "1px solid black"
-        }
-
-        if (provinceTrovate) {
-            // Se trova i dati, cambia il titolo
-            titoloRegione.textContent = "Province della regione " + nomeRegione + ":";
-
-            // Crea i punti elenco per ogni provincia
-            let table = elencoProvince.innerHTML
-            table = `<tr><th class="el">Nome</th><th class="el">Sigla</th><th class="el">Popolazione</th></tr>`
-            for (let provincia of provinceTrovate) {
-                if (provincia.metropolitana) {
-                    table += `<tr><td class="el nome"><strong>${provincia.nome}</strong></td><td class="el sigla"><strong>${provincia.sigla}</strong></td><td class="el abitanti"><strong>${provincia.abitanti.toLocaleString('it-IT')}</strong></td></tr>`;
-                }
-                else {
-                    table += `<tr><td class="el nome">${provincia.nome}</td><td class="el sigla">${provincia.sigla}</td><td class="el abitanti">${provincia.abitanti.toLocaleString('it-IT')}</td></tr>`;
-                }
-            }
-            elencoProvince.innerHTML = table
-
-        } else {
-            // Se clicchi una regione non ancora presente nel database
-            titoloRegione.textContent = nomeRegione + " (Dati da inserire)";
-        }
-    });
+        provinceCorrenti = provinceTrovate;
+        scritturaProvince(regioneCorrente, provinceCorrenti);
+        flagAlfabetico = true;
+        flagAbitanti = false;
+    })
 });
+
+function ordinaPerAbitantiDecrescente(provinceCorrenti) {
+    let copiaProvince = [...provinceCorrenti];
+    copiaProvince.sort((a, b) => {
+        return a.abitanti - b.abitanti;
+    })
+    scritturaProvince(regioneCorrente, copiaProvince);
+}
+
+function ordinaPerAbitantiCrescente(provinceCorrenti) {
+    let copiaProvince = [...provinceCorrenti];
+    copiaProvince.sort((a, b) => {
+        return b.abitanti - a.abitanti;
+    })
+    scritturaProvince(regioneCorrente, copiaProvince);
+}
+
+function ordinaPerAbitanti(flag) {
+    if (flag) {
+        ordinaPerAbitantiDecrescente(provinceCorrenti);
+        flagAbitanti = false;
+        flagAlfabetico = false;
+    }
+    if (!flag) {
+        ordinaPerAbitantiCrescente(provinceCorrenti);
+        flagAbitanti = true;
+        flagAlfabetico = false;
+    }
+}
+
+function ordinaPerNome(flag) {
+    if (flag) {
+        scritturaProvince(regioneCorrente, provinceCorrenti.toReversed());
+        flagAlfabetico = false;
+        flagAbitanti = false;
+    }
+    if (!flag) {
+        scritturaProvince(regioneCorrente, provinceCorrenti);
+        flagAlfabetico = true;
+        flagAbitanti = false;
+    }
+}
